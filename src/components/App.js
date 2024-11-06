ccc5ad9b9c17711118275868acd06d676833ddb2
Karimi-F <fridah37karimi@gmail.com>
2024-11-06 21:13:09 +0300

    Adds the filter function

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,43 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import Filter from "./Filter";
+import ItemForm from "./ItemForm";
 
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [search,setSearch] = useState("");
+  const [selectedCategory,setSelectedCategory] = useState("All")
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleSearchChange (search) {
+    setSearch (search);
+  }
+
+  function handleCategoryChange(selectedCategory){
+    setSelectedCategory(selectedCategory);
+  }
+
+function handleItemFormSubmit(newItem){
+  setItems((previousItems) =>[...previousItems,newItem]);
+}
+
+const filteredItems = items.filter((item) =>
+item.name.toLowerCase().includes(search.toLowerCase()));
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <Filter 
+        search={search} 
+        onSearchChange={handleSearchChange} 
+        onCategoryChange={handleCategoryChange}/>
+      <ItemForm onItemFormSubmit={handleItemFormSubmit}/>
+       <ShoppingList items={filteredItems} search={search} />
     </div>
   );
 }
--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,59 @@
 import React from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+
+
+function ItemForm({onItemFormSubmit}) {
+
+  const [itemName,setItemName] = React.useState("");
+  const [itemCategory, setItemCategory] = React.useState("Produce");
+  // const [addItem,setAddItem] = React.useState(newItem)
+
+function handleItemChange (event){
+  setItemName(event.target.value);
+}
+
+function handleCategoryChange(event){
+  setItemCategory(event.target.value);
+}
+
+console.log(onItemFormSubmit);
+
+  function handleSubmit(event){
+    event.preventDefault();
+
+    const newItem = {
+      id: uuid(),
+      name: itemName,
+      category: itemCategory,
+    };
+  
+    onItemFormSubmit(newItem);
+  
+    setItemName("");
+    setItemCategory("produce");
+  }
+
+ 
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+        type="text" 
+        name="name" 
+        value={itemName}
+        onChange={handleItemChange}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+        name="category" 
+        value={itemCategory} 
+        onChange={handleCategoryChange}
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
